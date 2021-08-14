09d94da5e6f13ca567ef8374a5eb3dd2b822f583
Kashyap Maheshwari <k.maheshwari@energycortex.com>
2021-08-15 00:15:22 +0200

    first version of search string textbox & join button #1

--- src/pages/main.py
+++ src/pages/main.py
@@ -1,13 +1,22 @@
 import streamlit as st
 
 AVAILABLE_DATABASES = [
-    'ACM',
-    'arXiv',
-    'bioRxiv',
-    'IEEE',
-    'medRxiv',
-    'PubMed',
-    'Scopus'
+    "ACM",
+    "arXiv",
+    "bioRxiv",
+    "IEEE",
+    "medRxiv",
+    "PubMed",
+    "Scopus"
+]
+
+JOIN_TYPES = [
+    "AND",
+    "OR",
+    "(AND",
+    "AND)",
+    "(OR",
+    "OR)"
 ]
 
 
@@ -32,10 +41,26 @@ def write():
             options=AVAILABLE_DATABASES
         )
 
-    st.subheader("Date Picker")
+    st.subheader("Date Picker :calendar:")
     col1, col2 = st.columns(2)
     start_date = col1.date_input("start date")
     end_date = col2.date_input("end date")
+
+    # https://blog.streamlit.io/session-state-for-streamlit/
+    st.subheader("Search string :question:")
+    str_col1, str_col2 = st.columns([3, 1])
+    search_string = str_col1.text_input(
+        "Please enter the search string", "")
+    join_type = str_col2.selectbox(
+        "Please select a type of join", JOIN_TYPES)
+    add_button = st.button("Add")
+    if "query_string" not in st.session_state:
+        st.session_state.query_string = []
+    if add_button:
+        st.session_state.query_string.append(
+            "[" + search_string + "]" + " " + join_type)
+    st.write("**Query string** :astonished:")
+    st.write(str(st.session_state.query_string))
 
     st.sidebar.subheader("Please enter the following APIKeys")
     ieee_api_key = st.sidebar.text_input("IEEE APIKey", type="password")
