480ddca42320f103f652578a9b37bb75784e18a3
Christan Gerloff <christian.gerloff@rwth-aachen.de>
2023-04-01 19:53:08 +0800

    fix: empty df #30

--- src/pages/1_1️⃣_search.py
+++ src/pages/1_1️⃣_search.py
@@ -12,7 +12,6 @@ from utils.search_engine import convert_search_to_json
 # configure page
 set_page_title("Literature Search")
 
-
 # sidebar
 st.sidebar.title("Search settings")
 
@@ -21,7 +20,11 @@ st.sidebar.write("We recommend using time-consuming enrich and cross-references 
                  "features only in console mode.")
 enrich_col, cross_search_col = st.sidebar.columns(2)
 enrich = enrich_col.checkbox("Enrich papers", value=False, help=cs.HELP_ENRICH)
-cross_search = cross_search_col.checkbox("Cross-references", value=False, help=cs.HELP_CROSS_REF)
+cross_search = cross_search_col.checkbox(
+    "Cross-references",
+    value=False,
+    help=cs.HELP_CROSS_REF
+)
 
 if enrich is True or cross_search is True:
     st.sidebar.info("We recommend using time-consuming enrich and" 
@@ -56,10 +59,12 @@ limit = st.sidebar.slider("Please select the maximum number of papers per databa
 
 # Duplication threshold here inverse definition
 st.sidebar.subheader("Duplication sensitivity")
-senitivity = st.sidebar.slider("Please select the maximum number of papers per database.",
-                               min_value=cs.DUPLICATION_MIN_SLIDER,
-                               max_value=cs.DUPLICATION_MAX_SLIDER,
-                               step=cs.DUPLICATION_STEP_SLIDER)
+senitivity = st.sidebar.slider(
+    "Please select the maximum number of papers per database.",
+    min_value=cs.DUPLICATION_MIN_SLIDER,
+    max_value=cs.DUPLICATION_MAX_SLIDER,
+    step=cs.DUPLICATION_STEP_SLIDER
+)
 similarity_threshold = 1 - (senitivity - cs.DUPLICATION_MIN_SLIDER)
 
 # progress bar
@@ -143,19 +148,12 @@ elif search_state and search_string != "":
     ris = fp.RisExport(search)
     ris_file, ris_df = ris.generate_ris()
 
-    # store session data
-    if 'review' not in st.session_state:
-        st.session_state.search = search
-        st.session_state.ris_df = ris_df.copy()
-        st.session_state.rayyan_df = rayyan_df.copy()
-        st.session_state.review = ris_df.copy()
-        st.session_state.review.insert(1, 'criteria', 'default')
-        st.session_state.review.insert(1, 'decision', True)
-        st.session_state.review.insert(1, 'reviewed', False)
-
+    # Export to RIS file
+    if ris_df is None:
+        st.warning("No search results to export!")
     else:
-        st.info("Override results!!!")
-        if st.button("Yes I'm ready to override"):
+        # store session data
+        if 'review' not in st.session_state:
             st.session_state.search = search
             st.session_state.ris_df = ris_df.copy()
             st.session_state.rayyan_df = rayyan_df.copy()
@@ -164,21 +162,32 @@ elif search_state and search_string != "":
             st.session_state.review.insert(1, 'decision', True)
             st.session_state.review.insert(1, 'reviewed', False)
 
-    # display results
-    st.dataframe(ris_df)
+        else:
+            st.info("Override results!!!")
+            if st.button("Yes I'm ready to override"):
+                st.session_state.search = search
+                st.session_state.ris_df = ris_df.copy()
+                st.session_state.rayyan_df = rayyan_df.copy()
+                st.session_state.review = ris_df.copy()
+                st.session_state.review.insert(1, 'criteria', 'default')
+                st.session_state.review.insert(1, 'decision', True)
+                st.session_state.review.insert(1, 'reviewed', False)
 
-    # download results
-    st.subheader("Download")
-    download_json, download_ris, download_csv, = st.columns(3)
-    download_json.download_button(label='Details - JSON',
-                                  data=result_json,
-                                  file_name='set_you_free_results.json',
-                                  mime='text/plain')
-    download_ris.download_button(label='CADIMA - RIS',
-                                 data=ris_file,
-                                 file_name='set_you_free_cadima.ris',
-                                 mime='text/plain')             
-    download_csv.download_button(label='Rayyan - CSV',
-                                 data=rayyan_file,
-                                 file_name='set_you_free_rayyan.csv',
-                                 mime='text/csv')
+        # display results
+        st.dataframe(ris_df)
+
+        # download results
+        st.subheader("Download")
+        download_json, download_ris, download_csv, = st.columns(3)
+        download_json.download_button(label='Details - JSON',
+                                    data=result_json,
+                                    file_name='set_you_free_results.json',
+                                    mime='text/plain')
+        download_ris.download_button(label='CADIMA - RIS',
+                                    data=ris_file,
+                                    file_name='set_you_free_cadima.ris',
+                                    mime='text/plain')             
+        download_csv.download_button(label='Rayyan - CSV',
+                                    data=rayyan_file,
+                                    file_name='set_you_free_rayyan.csv',
+                                    mime='text/csv')
