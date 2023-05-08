d78e4745a8d78b4fc782e7c79245c7903ff5c093
Christan Gerloff <christian.gerloff@rwth-aachen.de>
2023-05-08 13:21:12 +0800

    add: provide abstract directly #38

--- src/pages/2_2️⃣_study_selection.py
+++ src/pages/2_2️⃣_study_selection.py
@@ -3,29 +3,65 @@ import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode
 from utils.site_config import set_page_title
 
+def updata_review(data: pd.DataFrame,
+                  decision: bool = True,
+                  reasons: list = None):
+    """Update the review dataframe with the selected data.
+
+    Args:
+        data (pd.DataFrame): Dataframe with the selected data.
+        decision (bool, optional): Decision of the reviewer,
+            True means include, False means exclude. Defaults to True.
+        reasons (list, optional): Reasons for the decision.
+    """
+
+    if 'review' in st.session_state:                  
+        st.session_state.review.loc[
+            st.session_state.review.id == data['id'],
+            'reviewed'
+        ] = True
+        st.session_state.review.loc[
+            st.session_state.review.id == data['id'],
+            'decision'
+        ] = decision
+        st.session_state.review.loc[
+            st.session_state.review.id == data['id'],
+            'decision_reasons'
+        ] = reasons
 
 # configure page
-set_page_title("Study selection")
-
-st.subheader("Manual selection of publications")
+set_page_title("Study screening")
 
 if 'review' not in st.session_state:
     st.error("Please run the search first.")
-else:
-    # sidebar
-    st.sidebar.title("Inspection settings")
+    st.stop()
+    
+# sidebar
+st.sidebar.title("Inspection settings")
 
-    # criteria
-    criterias = st.sidebar.multiselect("Select one or more criterias:",
-                                       options=['default'],
-                                       default='default')
-    criterias = 'default' if criterias == '' else criterias
+# criteria
+criterias = st.sidebar.multiselect("Select one or more criterias:",
+                                   options=['default'],
+                                   default='default')
+criterias = 'default' if criterias == '' else criterias
 
-    st.sidebar.info(
-        f"Reviewed papers: {len(st.session_state.review[st.session_state.review.reviewed == True])} of "
-        f"{len(st.session_state.review)}"
-    )
+n_reviewed = len(
+    st.session_state.review[
+        st.session_state.review.reviewed == True
+    ]
+)
+st.sidebar.info(
+    f"Reviewed papers: {n_reviewed} of "
+    f"{len(st.session_state.review)}"
+)
 
+paper_pick = st.sidebar.radio(
+            "Pick paper for review:",
+            options=["auto", "manual"],
+)
+
+if paper_pick == "manual":
+    st.subheader("Manual selection of publications")
     # select study
     st.info(
         "You can use the checkbox in the table (see doi) to select a publication. "
@@ -53,33 +89,41 @@ else:
 
     st.session_state.review = grid['data']
     selected = grid['selected_rows']
-    selected_df = pd.DataFrame(selected)
+    selected_df = pd.DataFrame(selected).head(1)
+else:
+    # select first paper that is not reviewed
+    selected_df = st.session_state.review[
+        st.session_state.review.reviewed == False
+    ].head(1)
+with st.spinner("Load publication..."):
+    if not selected_df.empty:
+        st.markdown(f"## {selected_df.iloc[0]['title']} \n"
+                    f"***{selected_df.iloc[0]['doi']}***")
+        st.markdown("## Abstract \n"
+                    f"{selected_df.iloc[0]['abstract']}")
+        # exclusion
+        st.subheader("Decision...")
+        include_col, exclude_col = st.columns(2)
 
-    with st.spinner("Load publication..."):
-        if not selected_df.empty:
-            st.markdown(f"## {selected_df.loc[0, 'title']} \n"
-                        f"***{selected_df.loc[0, 'doi']}***")
-            st.markdown("## Abstract \n"
-                        f"{selected_df.loc[0, 'abstract']}")
+        decision_str = st.radio(
+            "Select the search type:",
+            options=["include", "exclude"]
+        )
+        decision = True if decision_str == "include" else False
 
-            # exclusion
-            st.subheader("Decision...")
-
-            exclude_col, submit_col = st.columns(2)
-
-            exclude = exclude_col.checkbox(f"Exclude because: {criterias}", value=False)
-
-            submit = submit_col.button("submit")
-
-            if submit:
-                st.session_state.review.loc[
-                    st.session_state.review.id == selected_df.loc[0, 'id'],
-                    'reviewed'
-                ] = True
-                st.session_state.review.loc[
-                    st.session_state.review.id == selected_df.loc[0, 'id'],
-                    'decision'
-                ] = not exclude
-
+        if decision:
+            reason = None
+            st.success("Paper will be included if you submit")
+        else:
+            reason = st.multiselect(
+                'Exlcusion reason:',
+                options=criterias,
+                default=criterias
+            )
+            st.error("Paper will be excluded if you submit")
         
-
+        submit = st.button(
+                 "Submit",
+                 on_click=updata_review,
+                 args=(selected_df, decision, reason)
+        )
