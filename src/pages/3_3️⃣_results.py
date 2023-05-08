9ae04743b38fd440ecfecd0241825bb0864f63e7
Christan Gerloff <christian.gerloff@rwth-aachen.de>
2023-05-08 13:26:00 +0800

    adj: PRISMA # 39
    + download button
    + st barchart

--- src/pages/3_3️⃣_results.py
+++ src/pages/3_3️⃣_results.py
@@ -1,4 +1,4 @@
-import numpy as np
+import pandas as pd
 import streamlit as st
 import graphviz as graphviz
 import matplotlib.pyplot as plt
@@ -8,6 +8,7 @@ from datetime import datetime
 from matplotlib_venn import venn2, venn3
 from utils.site_config import set_page_title
 from utils.search_engine import convert_search_to_json
+from utils.download import download_button
 
 # configure page
 set_page_title("Results of search")
@@ -17,11 +18,15 @@ if 'rayyan_df' not in st.session_state:
 elif 'review' in st.session_state:
     review = st.session_state.review.copy()
     final_search = st.session_state.search
-    papers = review.loc[review["decision"] == False, ['title', 'date', 'doi']]
+    papers = review.loc[
+        review["decision"] == False,
+        ['title', 'date', 'doi']
+    ]
 
     if not papers.empty:
         for _, p in papers.iterrows():
-            date = datetime.strptime(p.date, '%Y-%m-%d')
+            # cut all after T in date
+            date = datetime.strptime(p.date.split('T')[0], '%Y-%m-%d')
             paper_key = final_search.get_paper_key(
                 p.title, date, p.doi)
             paper = final_search.paper_by_key.get(paper_key, None)
@@ -40,7 +45,17 @@ elif 'review' in st.session_state:
     rayyan_selection = st.session_state.rayyan_df
     databases = st.session_state.databases
     all_papers = rayyan_selection.explode('databases')
-    stats_databses = all_papers.groupby(['databases'])['key'].apply(list)
+    
+    # count number papers per database for each database in `databases`
+    db_frame = all_papers.groupby(['databases'])['key'].count().to_frame()
+    # rename column
+    db_frame = db_frame.rename(columns={'key': 'number of papers'})
+    # add zero for missing databases
+    db_frame = db_frame.reindex(databases, fill_value=0)
+    # change type to int
+    db_frame = db_frame.astype(int)
+
+    # reduction by duplicates
     n_duplicates = len(all_papers) - len(rayyan_selection)
     
     if rayyan_df is None:
@@ -50,52 +65,80 @@ elif 'review' in st.session_state:
 
         prisma = graphviz.Digraph('PRISMA')
         prisma.attr('node', shape='box')
-        prisma.node('Identification')
-        prisma.node('Auto screening', f' {len(rayyan_selection)} records after duplicate removal')
-        prisma.node('Manual screening', f' {len(rayyan_df)} records after manual screeening')
-        prisma.edge('Identification', 'Auto screening', label=str(n_duplicates))
-        prisma.edge('Auto screening', 'Manual screening', label=str(n_removes))
+        with prisma.subgraph() as sub:
+            sub.attr(rank='same')
+            sub.node('Direct search',
+                     f'Direct database search\n (n={len(all_papers)})')
+            sub.node('Cross-references search',
+                     f'Cross-references search \n (n={0})')
+        with prisma.subgraph() as sub:
+            sub.attr(rank='same')
+            sub.node('Auto screening', 
+                        'Records after duplicates removed \n '
+                        f'(n={len(rayyan_selection)})')
+            sub.node('TiAbs',
+                     'Records excluded based on \n title and abstract screening \n '
+                     f'(n={n_removes})')
+        with prisma.subgraph() as sub:
+            sub.attr(rank='same')
+            sub.node('Manual screening', f'Records screened \n (n={len(rayyan_df)})')
+        
+        prisma.edge('Direct search', 'Auto screening') #label=str(n_duplicates))
+        prisma.edge('Cross-references search', 'Auto screening')
+        prisma.edge('Auto screening', 'TiAbs')
+        prisma.edge('Auto screening', 'Manual screening')
+
+        # save prisma
+        prisma_result = prisma.pipe(format='svg')
 
         prisma1_col, prisma2_col = st.columns(2)
         if len(databases) == 2:
-            venn2([set(stats_databses[databases[0]]),
-                set(stats_databses[databases[1]])],
-                set_labels=databases)
+            venn2(db_sets,
+                  set_labels=databases)
             prisma1_col.graphviz_chart(prisma)
             prisma2_col.pyplot(plt)
         elif len(databases) == 3:
-            venn3([set(stats_databses[databases[0]]),
-                set(stats_databses[databases[1]]),
-                set(stats_databses[databases[2]])],
-                set_labels=databases)
+            venn3(db_sets,
+                  set_labels=databases)
             prisma1_col.graphviz_chart(prisma)
             prisma2_col.pyplot(plt)
         else:
-            matches = []
-            prisma1_col.graphviz_chart(prisma)
-            unique_databases = [list(x) for x in set(tuple(x) for x in rayyan_df['databases'])]
-            for n in unique_databases:
-                matches.append(sum([n == i for i in rayyan_selection['databases']]))
-            y_pos = np.arange(len(unique_databases))
-            plt.bar(y_pos, matches)
-            plt.xticks(y_pos, unique_databases)
-            prisma2_col.pyplot(plt)
-
-
+            with prisma1_col:
+                st.graphviz_chart(prisma)
+            with prisma2_col:
+                st.bar_chart(
+                    data=db_frame,
+                    use_container_width=True
+                )
+        # download prisma
+        prisma_download_btn = download_button(
+                prisma_result,
+                'prisma.svg',
+                'Download flow diagram'
+            )
+        st.markdown(prisma_download_btn, unsafe_allow_html=True)
 
         # download review
-        st.subheader("Download review")
-
-        download_json, download_ris, download_csv, = st.columns(3)
-        download_json.download_button(label='Details - JSON',
-                                    data=result_json,
-                                    file_name='set_you_free_results.json',
-                                    mime='text/plain')
-        download_ris.download_button(label='CADIMA - RIS',
-                                    data=ris_file,
-                                    file_name='set_you_free_cadima.ris',
-                                    mime='text/plain')                   
-        download_csv.download_button(label='Rayyan - CSV',
-                                    data=rayyan_file,
-                                    file_name='set_you_free_rayyan.csv',
-                                    mime='text/csv')
+        st.subheader("Download results")
+        json_download_col, ris_download_col, csv_download_col = st.columns(3)
+        with json_download_col:
+            json_download_btn = download_button(
+                result_json,
+                'set_you_free_results.json',
+                'Details - JSON'
+            )
+            st.markdown(json_download_btn, unsafe_allow_html=True)
+        with ris_download_col:
+            ris_download_btn = download_button(
+                ris_file,
+                'set_you_free_cadima.ris',
+                'CADIMA - RIS'
+            )
+            st.markdown(ris_download_btn, unsafe_allow_html=True)
+        with csv_download_col:
+            csv_download_btn = download_button(
+                rayyan_file,
+                'set_you_free_rayyan.csv',
+                'Rayyan - CSV'
+            )
+            st.markdown(csv_download_btn, unsafe_allow_html=True)
