339d11b11783748c0eea1fe2ada0868c0bbb50fc
Kashyap Maheshwari <k.maheshwari@simplicity.ag>
2023-06-09 00:01:14 +0200

    added review & hash methods, and new params to paper model and format the module #34

--- findpapers/models/paper.py
+++ findpapers/models/paper.py
@@ -3,14 +3,17 @@ from datetime import date
 from typing import List, Optional, Union
 
 from pydantic import BaseModel, Field, validator
-from findpapers.models.publication import Publication
+
 from findpapers.data.available_databases import AVAILABLE_DATABASES
+from findpapers.models.publication import Publication
 
 
 class Paper(BaseModel):
     title: str = Field(..., examples="Fake title", description="Title of the paper.")
     abstract: str = Field(
-        ..., examples="Fake abstract", description="Abstract of the paper."
+        ...,
+        examples="Fake abstract",
+        description="Abstract of the paper.",
     )
     authors: List[str] = Field(
         ...,
@@ -19,7 +22,9 @@ class Paper(BaseModel):
     )
     publication: Publication
     publication_date: date = Field(
-        ..., examples="2020-01-01", description="Publication date of the paper."
+        ...,
+        examples="2020-01-01",
+        description="Publication date of the paper.",
     )
     urls: set[str] = Field(
         ...,
@@ -27,30 +32,53 @@ class Paper(BaseModel):
         description="URLs of the paper.",
     )
     doi: Optional[str] = Field(
-        None, examples="10.1016/j.jneumeth.2020.01.001", description="DOI of the paper."
+        None,
+        examples="10.1016/j.jneumeth.2020.01.001",
+        description="DOI of the paper.",
     )
     citations: Optional[int] = Field(
-        None, examples=[10], description="Number of citations of the paper."
+        None,
+        examples=[10],
+        description="Number of citations of the paper.",
     )
     keywords: Optional[set[str]] = Field(
-        None, examples=["Term A, Term B"], description="The keywords of the paper."
+        None,
+        examples=["Term A, Term B"],
+        description="The keywords of the paper.",
     )
     comments: Optional[str] = Field(
-        None, examples=["some comments"], description="The comments on the paper."
+        None,
+        examples=["some comments"],
+        description="The comments on the paper.",
     )
     number_of_pages: Optional[int] = Field(
-        None, examples=[5], description="The number of pages of the paper."
+        None,
+        examples=[5],
+        description="The number of pages of the paper.",
     )
     pages: Optional[str] = Field(
-        None, examples=["1-5"], description="The pages of the paper."
+        None,
+        examples=["1-5"],
+        description="The pages of the paper.",
     )
     databases: Optional[set[str]] = Field(
         None,
         examples=["IEEE", "ACM"],
         description="The databases where the paper is available.",
     )
+    reviewed: Optional[bool] = Field(
+        False,
+        description="Whether the paper is reviewed or not.",
+    )
+    criteria: Optional[list] = Field(
+        None,
+        examples=["some criteria"],
+        description="The criteria used to review the paper.",
+    )
     selected: Optional[bool] = Field(
-        None, examples=[True], description="Whether the paper is selected or not."
+        None,
+        examples=[True],
+        description="Whether the paper is selected or not.",
     )
     categories: Optional[bool] = Field(
         None,
@@ -58,14 +86,23 @@ class Paper(BaseModel):
         description="The categories of the paper.",
     )
     references: Optional[list] = Field(
-        [], examples=["Ref 1"], description="The references of the paper."
+        [],
+        examples=["Ref 1"],
+        description="The references of the paper.",
     )
     cites: Optional[list] = Field(
-        [], examples=["Citation 1"], description="The cites of the paper."
+        [],
+        examples=["Citation 1"],
+        description="The cites of the paper.",
     )
     source: str = Field(
-        default="primary", examples=["primary"], description="Source of the paper."
+        default="primary",
+        examples=["primary"],
+        description="Source of the paper.",
     )
+
+    def __hash__(self) -> int:
+        return self.title.__hash__()
 
     @validator("title")
     def check_title(cls, value: str) -> str:
@@ -93,10 +130,15 @@ class Paper(BaseModel):
         if value not in sources:
             raise (
                 ValueError(
-                    f"Source of the paper is invalid. Valid sources are {sources}."
+                    f"Source of the paper is invalid. Valid sources are {sources}.",
                 )
             )
         return value
+
+    def review(self, selected: bool, criteria: list = []) -> None:
+        self.reviewed = True
+        self.criteria = criteria
+        self.selected = selected
 
     def add_database(self, database_name: str) -> None:
         if database_name not in AVAILABLE_DATABASES:
@@ -107,55 +149,42 @@ class Paper(BaseModel):
         self.urls.add(url)
 
     def enrich(self, paper: "Paper") -> "Paper":
-        self.publication_date = (
-            paper.publication_date
-            if self.publication_date is None
-            else self.publication_date
-        )
+        self.publication_date = paper.publication_date if self.publication_date is None else self.publication_date
         self.doi = paper.doi if self.doi is None else self.doi
         self.abstract = (
             paper.abstract
-            if self.abstract is None
-            or (paper.abstract is not None and len(paper.abstract) > len(self.abstract))
+            if self.abstract is None or (paper.abstract is not None and len(paper.abstract) > len(self.abstract))
             else self.abstract
         )
         self.authors = (
             paper.authors
-            if self.authors is None
-            or (paper.authors is not None and len(paper.authors) > len(self.authors))
+            if self.authors is None or (paper.authors is not None and len(paper.authors) > len(self.authors))
             else self.authors
         )
         self.citations = (
             paper.citations
-            if self.citations is None
-            or (paper.citations is not None and paper.citations > self.citations)
+            if self.citations is None or (paper.citations is not None and paper.citations > self.citations)
             else self.citations
         )
         self.keywords = (
             paper.keywords
-            if self.keywords is None
-            or (paper.keywords is not None and len(paper.keywords) > len(self.keywords))
+            if self.keywords is None or (paper.keywords is not None and len(paper.keywords) > len(self.keywords))
             else self.keywords
         )
         self.comments = (
             paper.comments
-            if self.comments is None
-            or (paper.comments is not None and len(paper.comments) > len(self.comments))
+            if self.comments is None or (paper.comments is not None and len(paper.comments) > len(self.comments))
             else self.comments
         )
         self.number_of_pages = (
             paper.number_of_pages
             if self.number_of_pages is None
-            or (
-                paper.number_of_pages is not None
-                and paper.number_of_pages > self.number_of_pages
-            )
+            or (paper.number_of_pages is not None and paper.number_of_pages > self.number_of_pages)
             else self.number_of_pages
         )
         self.pages = (
             paper.pages
-            if self.pages is None
-            or (paper.pages is not None and len(paper.pages) > len(self.pages))
+            if self.pages is None or (paper.pages is not None and len(paper.pages) > len(self.pages))
             else self.pages
         )
 
@@ -171,14 +200,8 @@ class Paper(BaseModel):
             self.publication.enrich(paper.publication)
 
     def get_citation_key(self) -> str:
-        author_key = (
-            self.authors[0].lower().replace(" ", "").replace(",", "")
-            if len(self.authors) > 0
-            else "unknown"
-        )
-        year_key = (
-            self.publication_date.year if self.publication_date is not None else "XXXX"
-        )
+        author_key = self.authors[0].lower().replace(" ", "").replace(",", "") if len(self.authors) > 0 else "unknown"
+        year_key = self.publication_date.year if self.publication_date is not None else "XXXX"
         title_key = self.title.split(" ")[0].lower()
         citation_key = re.sub(r"[^\w\d]", "", f"{author_key}{year_key}{title_key}")
         return citation_key
