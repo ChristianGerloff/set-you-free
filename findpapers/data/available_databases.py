899ecb07d5e0d478d59e8d60b4212d07e7871a62
Kashyap Maheshwari <k.maheshwari@simplicity.ag>
2023-06-09 15:10:42 +0200

    update the available_databases module, add subject_area_by_key module & rename requests_utils module

--- findpapers/data/available_databases.py
+++ findpapers/data/available_databases.py
@@ -1,12 +1,23 @@
-# TODO: can this be implemented in a better way?
+from findpapers.searchers import (
+    acm_searcher,
+    arxiv_searcher,
+    biorxiv_searcher,
+    cross_ref_searcher,
+    ieee_searcher,
+    medrxiv_searcher,
+    opencitations_searcher,
+    pubmed_searcher,
+    scopus_searcher,
+)
+
 AVAILABLE_DATABASES: list[str] = [
-    "Scopus",
-    "IEEE",
-    "PubMed",
-    "arXiv",
-    "ACM",
-    "medRxiv",
-    "bioRxiv",
-    "OC",
-    "CR",
+    scopus_searcher.DATABASE_LABEL,
+    ieee_searcher.DATABASE_LABEL,
+    pubmed_searcher.DATABASE_LABEL,
+    arxiv_searcher.DATABASE_LABEL,
+    acm_searcher.DATABASE_LABEL,
+    medrxiv_searcher.DATABASE_LABEL,
+    biorxiv_searcher.DATABASE_LABEL,
+    opencitations_searcher.DATABASE_LABEL,
+    cross_ref_searcher.DATABASE_LABEL,
 ]
