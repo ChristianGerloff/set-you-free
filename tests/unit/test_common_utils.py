80ce3638b4d83eceaac91f586c85223f334b8505
Kashyap Maheshwari <k.maheshwari@simplicity.ag>
2023-06-11 14:31:18 +0200

    move the tests of models into a separate folder and parameterize the test of common utils

--- tests/unit/test_common_utils.py
+++ tests/unit/test_common_utils.py
@@ -1,22 +1,23 @@
+import pytest
+
 from findpapers.utils.common_utils import get_numeric_month_by_string
 
 
-def test_get_numeric_month_by_string_when_ful_form_of_string_is_provided() -> None:
-    assert get_numeric_month_by_string(string="January") == "01"
+@pytest.mark.parametrize(("month", "expected_output"), [("January", "01"), ("March", "03"), ("October", "10")])
+def test_get_numeric_month_by_string_when_full_form_of_string_is_provided(month: str, expected_output: str) -> None:
+    assert get_numeric_month_by_string(string=month) == expected_output
 
 
-def test_get_numeric_month_by_string_when_short_form_of_string_is_provided() -> None:
-    assert get_numeric_month_by_string(string="Mar") == "03"
+@pytest.mark.parametrize(("month", "expected_output"), [("Feb", "02"), ("May", "05"), ("Nov", "11")])
+def test_get_numeric_month_by_string_when_short_form_of_string_is_provided(month: str, expected_output: str) -> None:
+    assert get_numeric_month_by_string(string=month) == expected_output
 
 
-def test_get_numeric_month_by_string_when_number_is_provided_as_string() -> None:
-    assert get_numeric_month_by_string(string="12") == "12"
+@pytest.mark.parametrize(("month", "expected_output"), [("06", "06"), ("08", "08"), ("11", "11")])
+def test_get_numeric_month_by_string_when_month_is_provided_as_number(month: str, expected_output: str) -> None:
+    assert get_numeric_month_by_string(string=month) == expected_output
 
 
-def test_get_numeric_month_by_string_when_string_is_not_valid() -> None:
-    assert get_numeric_month_by_string(string="15") == "01"
-    assert get_numeric_month_by_string(string="fdgdf") == "01"
-
-
-def test_get_numeric_month_by_string_when_string_is_not_provided() -> None:
-    assert get_numeric_month_by_string(string=None) == "01"
+@pytest.mark.parametrize("string", ["15", "fdgdf", None])
+def test_get_numeric_month_by_string_when_string_is_not_valid(string: str) -> None:
+    assert get_numeric_month_by_string(string=string) == "01"
